e8b7a277a6288c3161d7b918cb6b227d21f15b67
nghia nguyen <futuregohan1997@gmail.com>
2020-10-30 11:31:32 +0700

    make it better

---
+++ get_all_interest.py
@@ -0,0 +1,29 @@
+import requests
+import io
+
+
+def write_file_line(file_name,line):
+    f = io.open(file_name,mode='a')
+    f.write('%s\n'%line)
+    f.close()
+
+
+
+token = 'EAAFFZBH9rZB4wBALzsZBZBL9OwQ5iitVJTD2ZBfF2vk85aZB2GQl4NZBTm3SlumvkAJyZAygugL2q8h8eiNFWcN1crLpgL99X7fpes1EjF179gU5kr0ZBvbJZCeATHAmyMYGgk4MOdBzbClalJ4ZCZAScndfbmFGMg93Pmk0IHNwGQCwvQZDZD'
+url = 'https://graph.facebook.com/v8.0/search?type=adTargetingCategory&class=interests&access_token=%s'%token
+data = requests.get(url).json()
+
+# len(data['data'])
+
+for ele in data['data']:
+    path = ele['path']
+    print(path)
+    
+    line = '"'
+    line += '","'.join(path)
+    line += '"'
+
+    write_file_line('file_name.csv',line)
+
+
+# write_file_line('file_name.txt','line')
---
+++ utils_db.py
@@ -0,0 +1,2 @@
+SELECT `id`, `name_en`, `name_vi`, `id_api`, `id_cha`, `lv` FROM `target_interest` WHERE 1
+
