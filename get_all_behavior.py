64cbde4c90e7ebdc6b30fd74c6ea1b29f235e5fd
nghia nguyen <futuregohan1997@gmail.com>
2020-10-30 11:37:58 +0700

    make it better

--- get_all_behavior.py
+++ get_all_behavior.py
@@ -10,7 +10,7 @@ def write_file_line(file_name,line):
 
 
 token = 'EAAFFZBH9rZB4wBALzsZBZBL9OwQ5iitVJTD2ZBfF2vk85aZB2GQl4NZBTm3SlumvkAJyZAygugL2q8h8eiNFWcN1crLpgL99X7fpes1EjF179gU5kr0ZBvbJZCeATHAmyMYGgk4MOdBzbClalJ4ZCZAScndfbmFGMg93Pmk0IHNwGQCwvQZDZD'
-url = 'https://graph.facebook.com/v8.0/search?type=adTargetingCategory&class=interests&access_token=%s'%token
+url = 'https://graph.facebook.com/v8.0/search?type=adTargetingCategory&class=behaviors&access_token=%s'%token
 data = requests.get(url).json()
 
 # len(data['data'])
@@ -24,7 +24,7 @@ for ele in data['data']:
     line += '","'.join(path)
     line += '"'
 
-    write_file_line('interst.csv',line)
+    write_file_line('behavior.csv',line)
 
 
 # write_file_line('file_name.txt','line')
